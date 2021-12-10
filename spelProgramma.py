7e30ef71e1490d209c5d8d0453e7bd2c4d92113e
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-12-10 09:33:33 +0100

    Finished collections

--- spelProgramma.py
+++ spelProgramma.py
@@ -1,4 +1,13 @@
+import random
+
 spelList = ('Monopoly', 'Yahtzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo')
 
-def spelProgramma(spelList):
-    
+
+def spelProgramma(spelList,minimum = 3, maximum = 10):
+    x = random.choice(range(minimum,maximum))
+    return random.choices(spelList, k=x)
+
+
+print(spelProgramma(spelList))
+print(spelProgramma(spelList, 1))
+print(spelProgramma(spelList, 3))
